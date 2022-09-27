bb408dfc005ebdde757ae3f1ec6df2fadee355a3
filippova_t <tanya-fef@yandex.ru>
2022-09-27 23:50:27 +0800

    домашка 2.7

---
+++ src/main/java/pro/sky/course2/HomeworkCourse2number7/HomeworkCourse2number7Application.java
@@ -0,0 +1,13 @@
+package pro.sky.course2.HomeworkCourse2number7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkCourse2number7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkCourse2number7Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/course2/HomeworkCourse2number7/HomeworkCourse2number7ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.course2.HomeworkCourse2number7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkCourse2number7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
